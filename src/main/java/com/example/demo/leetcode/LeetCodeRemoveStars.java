3b9b6cffd212c1e2b26c4f7c3b8061d21e1c63c2
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2023-06-18 21:22:36 +0100

    LeetCode remove stars

--- src/main/java/com/example/demo/leetcode/LeetCodeRemoveStars.java
+++ src/main/java/com/example/demo/leetcode/LeetCodeRemoveStars.java
@@ -1,11 +1,31 @@
 package com.example.demo.leetcode;
 
+import java.nio.charset.CharsetEncoder;
 import java.util.Stack;
 
 public class LeetCodeRemoveStars {
     public String removeStars(String s) {
-        Stack<String> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i)=='*'){
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        char c[] = new char[stack.size()];
+        for(int i=c.length-1; i>=0; i--){
+            c[i]=stack.pop();
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int j = 0; j<c.length; j++){
+            sb.append(c[j]);
+        }
+        return sb.toString();
+    }
 
-        return s;
+    public static void main(String[] args) {
+        LeetCodeRemoveStars removeStars = new LeetCodeRemoveStars();
+        System.out.println(removeStars.removeStars("leet**cod*e"));
     }
 }
