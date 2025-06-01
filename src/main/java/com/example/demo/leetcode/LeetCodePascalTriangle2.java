7de97ba08d340930efb4422adf8a27e47a6e0126
ARIJIT CHAUDHURY <34763856+arijit-chaudhury@users.noreply.github.com>
2025-06-01 21:04:49 +0100

    Pascal Triangle2

--- src/main/java/com/example/demo/leetcode/LeetCodePascalTriangle2.java
+++ src/main/java/com/example/demo/leetcode/LeetCodePascalTriangle2.java
@@ -10,25 +10,24 @@ public class LeetCodePascalTriangle2 {
     }
 
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
-        if (numRows == 0) return triangle;
-
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
-
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> prev_row = triangle.get(i - 1);
-            List<Integer> new_row = new ArrayList<>();
-
-            for (int j = 1; j < prev_row.size(); j++) {
-                System.out.println("inside for loop j-1 "+prev_row.get(j - 1));
-                System.out.println("inside for loop j "+prev_row.get(j));
-                new_row.add(prev_row.get(j - 1) + prev_row.get(j));
-            }
-            new_row.add(1);
-            triangle.add(new_row);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        curr.add(1);
+        ans.add(new ArrayList<>(curr));
+        if(numRows == 1){
+            return ans;
         }
 
-        return triangle;
-    }
+        for(int rows = 1;rows<numRows;rows++){
+            List<Integer> prev = curr;
+            curr = new ArrayList<>();
+            curr.add(1);
+            for(int i=1;i<prev.size();i++){
+                curr.add(prev.get(i)+prev.get(i-1));
+            }
+            curr.add(1);
+            ans.add(new ArrayList<>(curr));
+        }
+        return ans;
+        }
 }
